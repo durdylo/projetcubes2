62e4e5ff6dcf0e9264ffd10ee7fa7b96a1876825
phaeryl <florian.arnaud2019@gmail.com>
2021-02-18 10:54:02 +0100

    Début graphique données

---
+++ ajax.php
@@ -0,0 +1,33 @@
+<script>
+
+var getHttpRequest = function () {
+  var httpRequest = false;
+
+  if (window.XMLHttpRequest) { // Mozilla, Safari,...
+    httpRequest = new XMLHttpRequest();
+    if (httpRequest.overrideMimeType) {
+      httpRequest.overrideMimeType('text/xml');
+    }
+  }
+  else if (window.ActiveXObject) { // IE
+    try {
+      httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+    }
+    catch (e) {
+      try {
+        httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+      catch (e) {}
+    }
+  }
+
+  if (!httpRequest) {
+    alert('Erreur : Impossible de générer une instance XMLHTTP');
+    return false;
+  }
+
+  return httpRequest
+} 
+
+xmlHttpRequest = getHttpRequest
+</script>
